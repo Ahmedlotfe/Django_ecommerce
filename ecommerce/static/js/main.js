d932aafc3d0e6510f3f2c626a3b71db640fcc0ee
Ahmedlotfe <ahmedlotfe132@gmail.com>
2021-09-18 17:08:49 +0200

    Uploading Project

---
+++ ecommerce/static/js/main.js
@@ -0,0 +1,41 @@
+let updateBtns = document.getElementsByClassName('update-cart')
+
+for (let i = 0; i < updateBtns.length; i++) {
+    updateBtns[i].addEventListener('click', function () {
+        let productId = this.dataset.product
+        let action = this.dataset.action
+        console.log(`ProductId: ${productId}, Action: ${action}`)
+        console.log(`USER: ${user}`)
+
+        if (user == 'AnonymousUser') {
+            console.log('User is not authenticated')
+        } else {
+            updateUserOrder(productId, action)
+        }
+
+    })
+}
+
+function updateUserOrder(productId, action) {
+    console.log('User is authenticated, sending data..')
+
+    url = '/update_item/'
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken
+        },
+        body:JSON.stringify({ 'productId': productId, 'action': action })
+    })
+
+        .then((response) => {
+            return response.json();
+        })
+        
+        .then((data) => {
+            console.log('Data:', data)
+            location.reload()
+        });
+}
