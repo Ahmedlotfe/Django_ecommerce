c5b4daceb5ac808b5c201ebd71e809d7e8db941b
Ahmedlotfe <ahmedlotfe132@gmail.com>
2021-10-12 15:02:12 +0200

    update

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -32,9 +32,17 @@ def cart(request):
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
+        try:
+            cart = json.loads(request.COOKIES['cart'])
+        except:
+            cart = {}
+        print(f'Cart: {cart}')
         items = []
         order = {'get_cart_total': 0, 'get_cart_items':0, 'shipping': False}
         cartItems = order['get_cart_items']
+        
+        for i in cart:
+            cartItems += cart[i]['quantity']
     context = {
         'items': items,
         'order': order,
