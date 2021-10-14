bd63e82676fd88c1f590bc90d068e02c6bbf92c3
Ahmedlotfe <ahmedlotfe132@gmail.com>
2021-10-14 14:47:16 +0200

    updating

--- ecommerce/store/signals.py
+++ ecommerce/store/signals.py
@@ -1,16 +1,15 @@
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 from .models import Customer
 
+@receiver(post_save, sender=User)
 def create_customer(sender, instance, created, **kwargs):
     if created:
         Customer.objects.create(user=instance)
 
-post_save.connect(create_customer, sender=User)
 
-
+@receiver(post_save, sender=User)
 def update_customer(sender, instance, created, **kwargs):
     if created == False:
         instance.customer.save()
-
-post_save.connect(update_customer, sender=User)
